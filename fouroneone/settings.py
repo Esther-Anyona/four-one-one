e4bfd6e29c5f6d840dc5c22e4df6d1fda8081c5c
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-19 00:30:14 +0300

    Keep the secret key safe

--- fouroneone/settings.py
+++ fouroneone/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-il-=eeo6^5l329f8lh)ag$77qsg)7nx$jfbpr+%p5g2&zp2zm3'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
