ba167093518c8d4d442cb0d42679ce9a53e79789
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-20 23:27:07 +0300

    Add registration details

--- newsapp/views.py
+++ newsapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 
-# Create your views here.
-@login_required(login_url='/accounts/login/')
+"""
+Views here
+"""
 def home(request):
     return render(request, 'home.html')
