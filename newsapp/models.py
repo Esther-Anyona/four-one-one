65c7b31c9a37732a5ee2754c9898f297d82a435b
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-21 21:13:09 +0300

    Add models, add cloudinary, create superuser

--- newsapp/models.py
+++ newsapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# CloudinaryImage("turtles.jpg").image(width=70, height=53, crop="scale")
+"""
+Models 
+"""
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=50)
+    description = models.TextField()
+    occupants_count = models.IntegerField(null=True, blank=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood_pic = CloudinaryField('hood-pic')
+    ambulance_contact = models.CharField(max_length=20, null=True, blank=True)
+    police_contact = models.CharField(max_length=20, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    member_id = models.CharField(max_length=10, blank=True)
+    profile_pic = CloudinaryField('profile-pic')
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    biz_email = models.CharField(max_length=100)
+
+    """
+    methods
+    """
+    # create_business()
+    # delete_business()
+    # find_business(business_id)
+    # update_business()
+
+
+
