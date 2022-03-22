d3bee6f34d0e4a4668c41983b64afb0e44ff6ff6
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-22 09:21:40 +0300

    Add home template showing all neighbourhoods

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from .models import *
+from django.contrib.auth.models import User
+
 
 """
 Views here
 """
 def home(request):
-    return render(request, 'home.html')
+    neighbourhoods = Neighbourhood.objects.all()
+    return render(request, 'home.html', {'neighbourhoods':neighbourhoods})
